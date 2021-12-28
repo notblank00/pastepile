a5c73577e78d8acede3e1999a9e9a50ae5d076c7
notblank00 <pyromancy00@gmail.com>
2021-12-28 09:38:11 +0300

    fixed validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :pastes
-  validates_presence_of :username, :email, :password
+  validates_presence_of :username, :email, :password_digest
   validates_uniqueness_of :username, :email
   validates :password, confirmation: { case_sensitive: true }
 
